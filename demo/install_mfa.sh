781904404fd619c2f6703af24becefe23162d6f1
rohitmhnty <rohitkumarmahanty1234@gmail.com>
2025-03-02 05:07:53 +0530

    added demo folder

---
+++ demo/install_mfa.sh
@@ -0,0 +1,6 @@
+pip install git+https://github.com/RobinWitch/Montreal-Forced-Aligner.git
+conda install -c conda-forge kalpy
+pip install pgvector
+pip install Bio
+mfa model download acoustic english_us_arpa
+mfa model download dictionary english_us_arpa
